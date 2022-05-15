55a6b9621c91f9d22d64491e76836f96e1195ded
Rob <mccartney.r@gmail.com>
2022-05-15 01:05:50 -0700

    enable web dev tools

--- src/frontend/client.js
+++ src/frontend/client.js
@@ -4,6 +4,11 @@ const {KvStoreClient} = require('./kvprog_grpc_web_pb.js');
 var client = new KvStoreClient('http://localhost:8080');
 var request = new CallsRequest();
 
+const enableDevTools = window.__GRPCWEB_DEVTOOLS__ || (() => {});
+enableDevTools([
+  client,
+]);
+
 client.calls(request, {}, (err, response) => {
     console.log(response.getCallInfoList());
 });
