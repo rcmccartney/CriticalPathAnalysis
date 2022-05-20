6f006ab525a527e4577006d941dc8fd27e197b64
Bhavika Jain <bhavikap@buffalo.edu>
2022-05-19 20:47:08 -0700

    added dependency for d3

--- src/main/java/kvprog/bserver/BComponentModule.java
+++ src/main/java/kvprog/bserver/BComponentModule.java
@@ -9,8 +9,7 @@ import kvprog.bserver.ServerApp.ServerComponent;
 class BComponentModule {
 
   @Provides
-  static BImplServiceDefinition.Factory provideServiceFactor(
-      final ServerComponent component) {
+  static BImplServiceDefinition.Factory provideServiceFactor(final ServerComponent component) {
     return new BImplServiceDefinition.Factory() {
       @Override
       public BImplServiceDefinition grpcService(
