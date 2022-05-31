e1474b5c52e2c0c4c7513e1f75bccbb7c86b4229
Bhavika Jain <bhavikap@buffalo.edu>
2022-05-31 13:40:09 -0700

    generate CP using circles

--- src/frontend/client.js
+++ src/frontend/client.js
@@ -18,7 +18,7 @@ function getCostList() {
         });
 }
 
-const width = 900;
+const width = 1000;
 const height = 450;
 const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
@@ -26,44 +26,62 @@ function display (result) {
     const costELementList = result.map(element => element.getElementList());
     console.log(costELementList);
     const costElement = [];
-    costELementList[0].forEach(element => {
-        costElement.push({x:element.getSource(),y:element.getCostSec()});
-    });
-    const svg = d3.select('#bar-graph')
-        .append('svg')
-        .attr('width', width - margin.left - margin.right)
-        .attr('height', height - margin.top - margin.bottom)
-        .attr("viewBox", [0, 0, width, height]);
 
-    const xscale = d3.scaleBand()
-        .domain(d3.range(costElement.length))
-        .range([margin.left, width - margin.right])
-        .padding(0.1);
+    var firstElement = costELementList[0];
+    for(let i = 0; i < firstElement.length; i++) {
+        let index = firstElement[i].getSource().lastIndexOf("/");
+        const cost = firstElement[i].getCostSec();
+        const source = firstElement[i].getSource().substring(index+1);
+        costElement.push({source, cost});
+    }
+    console.log(costElement);
+    var svg = d3.select('#bar-graph').append('svg').
+                    attr('height', 450).attr('width', 2000);
 
-    const yscale = d3.scaleLinear()
-        .domain([0, 10])
-        .range([height - margin.bottom, margin.top]);
+    svg.selectAll('circle')
+            .data(costElement)
+            .enter().append('circle')
+            .attr('cx', function(d, i) {return 200 + (i * 80)})
+            .attr('cy','300')
+            .attr('r', 20)
+            .style('fill', 'green');
 
-    svg.append("g")
-        .attr("fill", 'royalblue')
-        .selectAll("rect")
-        .data(costElement)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => xscale(i))
-        .attr("y", d => yscale(d.y))
-        .attr("height", (d) => {return  yscale(0)-yscale(d.y)})
-        .attr("width", xscale.bandwidth());
+    var x =  220;
+    for(let i = 0; i < firstElement.length-1; i++) {
+        var x2 = x+40;
+            d3.select("svg")
+                .append("line")
+                .attr("x1", x)
+                .attr("y1", 300)
+                .attr("x2", x2)
+                .attr("y2", 300)
+                .attr("stroke", "red");
+        x = x2 + 40;
+    }
 
-    svg.append("g")
-        .attr("transform", `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(yscale).ticks(null, costElement.format))
-        .attr("font-size", '20px');
 
-    svg.append("g")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(xscale).tickFormat(i => costElement[i].x))
-        .attr("font-size", '20px');
+    /*let links = [];
+    // Link from the first node to the second
+    links.push(
+        d3.linkHorizontal()({
+            source: nodes[0],
+            target: nodes[1]
+        })
+    );
+    // Link from the first node to the third
+    links.push(
+        d3.linkHorizontal()({
+            source: nodes[0],
+            target: nodes[2]
+        })
+    );
+    // Append the links to the svg element
+    for (let i = 0; i < links.length; i++) {
+        svg
+            .append('path')
+            .attr('d', links[i])
+            .attr('stroke', 'black')
+            .attr('fill', 'none');*/
 
     return svg.node();
 }
