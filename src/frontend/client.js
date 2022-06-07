0d74487ea830bb34b2622b3fbbf91cd5318355a0
rcmccartney <mccartney.r@gmail.com>
2022-06-06 23:59:28 -0700

    fixed d3 visualization

--- src/frontend/client.js
+++ src/frontend/client.js
@@ -1,6 +1,6 @@
 const {CallsRequest} = require('./kvprog_pb.js');
 const {KvStoreClient} = require('./kvprog_grpc_web_pb.js');
-import * as d3 from "./d3.min.js"
+import * as d3 from "./d3.v4.js";
 
 let client = new KvStoreClient('http://localhost:8080');
 let request = new CallsRequest();
@@ -18,10 +18,10 @@ async function getCriticalPaths() {
         });
 }
 
-let data = getCriticalPaths()
+let cp_data = getCriticalPaths()
 
 async function addToDropdown () {
-    let data_present = await data;
+    let data_present = await cp_data;
     const list_element = document.getElementById("list");
     for (let i = 0; i < data_present.length; i++) {
         let option = document.createElement("option");
@@ -36,51 +36,120 @@ function slice (str, char) {
 }
 
 async function graph (path_number) {
-    let data_present = await data;
+    let data_present = await cp_data;
     const criticalPathElementListList = data_present.map(element => element.getElementList());
     const criticalPathElementList = criticalPathElementListList[path_number];
-    let pathElementNodes = [];
-    for(let i = 0; i < criticalPathElementList.length; i++) {
+
+    let nodes = [];
+    let links = [];
+    for (let i = 0; i < criticalPathElementList.length; i++) {
         const str = slice(slice(slice(criticalPathElementList[i].getSource(), "/"), "."), "_");
         const cost = criticalPathElementList[i].getCostSec();
-        pathElementNodes.push(cost);
+        let node = {"id" : i+1, "name" : str + ": " + cost }
+        nodes.push(node);
+        if (i+1 != criticalPathElementList.length) {
+            let link = {"source": i + 1, "target": i + 2}
+            links.push(link);
+        }
     }
-    console.log(pathElementNodes);
-    d3.select('#graph').select("*").remove();
-    var svg = d3.select('#graph')
-        .append('svg')
-        .attr('height', 450)
-        .attr('width', 2000);
 
-    svg.selectAll('circle')
-        .data(pathElementNodes)
-        .enter().append('circle')
-        .attr('cx', function(d, i) {return 200 + (i * 80)})
-        .attr('cy','300')
-        .attr('r', 20)
-        .style('fill', 'green');
+    let data = { "nodes": nodes, "links": links };
 
-    var x =  220;
-    for (let i = 0; i < criticalPathElementList.length-1; i++) {
-         var x2 = x+40;
-         var line =  d3.select("svg")
-                .append("line")
-                .attr("x1", x)
-                .attr("y1", 300)
-                .attr("x2", x2)
-                .attr("y2", 300)
-                .attr("stroke", "red")
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 2000 - margin.left - margin.right,
+    height = 1000 - margin.top - margin.bottom;
 
-        line.append('text')
-            .attr('class', 'barsEndlineText')
-            .attr('text-anchor', 'middle')
-            .text('eg');
-        x = x2 + 40;
+    d3.selectAll('svg').remove();
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#my_dataviz")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    // Initialize the links
+    var link = svg
+        .selectAll("line")
+        .data(data.links)
+        .enter()
+        .append("line")
+        .style("stroke", "#aaa");
+
+    // Initialize the nodes
+    var node = svg
+        .selectAll("circle")
+        .data(data.nodes)
+        .enter()
+        .append("circle")
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended))
+        .attr("r", 20)
+        .style("fill", "#69b3a2");
+
+    var text = svg.selectAll("text")
+        .data(data.nodes)
+        .enter()
+        .append("text");
+
+    var textLabels = text
+        .text(function (d) {
+            return d.name
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("fill", "red");
+
+    var simulation = d3.forceSimulation(data.nodes)
+        .force("link", d3.forceLink()
+            .id(function(d) { return d.id; })
+            .links(data.links))
+        .force("charge", d3.forceManyBody().strength(-400))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .on("tick", ticked);
+
+    // This function is run at each iteration of the force algorithm, updating the nodes position.
+    function ticked() {
+        link
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        node
+            .attr("cx", function (d) { return d.x+6; })
+            .attr("cy", function(d) { return d.y-6; });
+
+        textLabels
+            .attr("x", function (d) { return d.x; })
+            .attr("y", function (d) { return d.y; });
+    }
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
     }
 }
 
 async function protoPrint(value) {
-    let data_present = await data;
+    let data_present = await cp_data;
     document.getElementById("json").textContent = JSON.stringify(data_present[value].array, undefined, 2);
 }
 
